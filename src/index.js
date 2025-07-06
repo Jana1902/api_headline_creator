8b95466ae1a6a65f481965d52275ed97ab9ece22
Jana1902 <jana7cr@gmail.com>
2025-07-06 18:15:47 +0530

    First commit

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://headline-generator-seven.vercel.app",
     credentials: true,
   })
 );
